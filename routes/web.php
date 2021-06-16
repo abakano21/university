a778d086ade99a8f8310258895b9436bfc10765c
Abakan <abakano21@gmail.com>
2021-06-16 08:36:06 +0400

    web file updated

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/secret-migration-update', function () {
-
-    $students = Student::all();
-
-    $class = 1;
-    $current = 1;
-
-    foreach ($students as $row) {
-        
-        $leftbound = $current;
-        $rightbound = $current + 3;
-
-        $row->where('id', '>=', $leftbound)
-            ->where('id', '<=', $rightbound)
-            ->update(['class' => $class]);
-
-        $current += 3;
-        $class +=1;
-    }
-});
